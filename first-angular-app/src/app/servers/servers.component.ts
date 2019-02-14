f68ee7a3e76a076ae02d0c6720ab5be0c31f4c2e
James Choi <jchoi@xe.com>
2019-02-14 10:21:41 -0500

    Angular udemy completed upto section 2 lecture 27.

--- first-angular-app/src/app/servers/servers.component.ts
+++ first-angular-app/src/app/servers/servers.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-servers',
+    selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServers: boolean = false;
+  serverCreationStatus: string = 'No server was created.';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
   }
 }
