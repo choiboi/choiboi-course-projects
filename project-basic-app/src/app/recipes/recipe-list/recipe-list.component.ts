cea40b2fa8065d7d816c05cd5dd5359e8dfda396
James Choi <james.choiboi@gmail.com>
2019-12-26 01:52:46 -0500

    Create recipe model and initial recipe layout.

--- project-basic-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-basic-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'A test recipe description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/800px-Foods_%28cropped%29.jpg')
+  ];
 
   constructor() { }
 
