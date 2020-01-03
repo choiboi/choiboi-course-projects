479264483843c54de91d0c126909750702981466
jchoi <james.choi@xe.com>
2020-01-02 23:14:34 -0500

    Host binding in a directive.

--- first-angular-app/src/app/better-highlight/better-hightlight.directive.ts
+++ first-angular-app/src/app/better-highlight/better-hightlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 // Used `ng g d better-highlight` to create this directive.
 @Directive({
@@ -8,11 +8,22 @@ import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
 // it will run on the browser where the DOM is rendered. This way allows styling
 // (in this case) to be applied even when DOM is not accessible (ie. SSR).
 export class BetterHightlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.backgroundColor = 'transparent';
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey');
+    this.backgroundColor = 'blue';
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'transparent';
   }
 }
