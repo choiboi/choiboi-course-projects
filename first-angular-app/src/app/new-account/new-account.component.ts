8ad70493f72bbdf2c6a12d4b1f551e444b9c1417
jchoi <james.choi@xe.com>
2020-01-07 15:48:03 -0500

    Component communication using EventEmitter via services.

--- first-angular-app/src/app/new-account/new-account.component.ts
+++ first-angular-app/src/app/new-account/new-account.component.ts
@@ -9,7 +9,11 @@ import { AccountsService } from '../services/accounts.service';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  constructor(private loggingService: LoggingService, private accountsService: AccountsService) { }
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe(
+      (status: string) => alert('New Status: ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
