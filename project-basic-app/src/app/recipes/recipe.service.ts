e9179503a50164030463b8981a6c4b3f2d9cbb98
jchoi <james.choi@xe.com>
2020-01-09 14:56:55 -0500

    Display list of ingredients.

--- project-basic-app/src/app/recipes/recipe.service.ts
+++ project-basic-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'A test recipe description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/800px-Foods_%28cropped%29.jpg'),
-    new Recipe('A Test Recipe 2', 'A test recipe 2 description', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/800px-Foods_%28cropped%29.jpg'),
+    new Recipe(
+      'Chicken Soup',
+      'A test recipe description',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/800px-Foods_%28cropped%29.jpg',
+      [
+        new Ingredient('Chicken', 2),
+        new Ingredient('Noodles', 1)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'A test recipe 2 description',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/800px-Foods_%28cropped%29.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Lettice', 2),
+        new Ingredient('Tomato', 1),
+        new Ingredient('Beef Patty', 2)
+      ]
+    ),
   ];
 
   getRecipes() {
