07edbfdec48310d815f3e5395781ffb8c6bf1770
James Choi <james.choiboi@gmail.com>
2020-08-27 19:54:44 -0400

    Implement mutation codes.

--- graphql-tutorial/start/client/src/index.tsx
+++ graphql-tutorial/start/client/src/index.tsx
@@ -1,17 +1,30 @@
 import {
   ApolloClient,
-  InMemoryCache,
   NormalizedCacheObject,
-  ApolloProvider
+  ApolloProvider,
+  HttpLink
 } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
+import { cache } from './cache';
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token')
+    }
+  })
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: []
+  }
 });
 
 injectStyles();
